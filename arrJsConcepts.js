741fbabd12d2459de42e0320fd031d88b52705f8
Freshia-Njoki <fnjokimacharia@gmail.com>
2023-12-28 20:58:45 +0300

    Math.floor and slice

--- arrJsConcepts.js
+++ arrJsConcepts.js
@@ -1,4 +1,4 @@
 let arr = [2,35,123,45,5,5,62];
 console.log(Math.floor(arr.length /2))
 console.log(arr.slice(0,3))
-console.log(arr.slice(3))//from the third element going forward
+console.log(arr.slice(3))//from the third element to the end
