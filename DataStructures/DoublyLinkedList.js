5d8b358f1aefd0a13fca3beb2c754fc470ce13c4
Freshia-Njoki <fnjokimacharia@gmail.com>
2024-01-03 22:16:08 +0300

    shift, unshift and get method

--- DataStructures/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList.js
@@ -22,6 +22,41 @@
 //decrement the length
 //return the value removed
 
+//shift - removes the node from the beginning of the D.L.L
+//Pseudocode
+//if the length is 0, return undefined
+//store the current head property in a variable - oldHead
+//if the length is 1:
+    //set the head to be null
+    //set the tail to be null
+//update the head to be the next of the old head
+//set the head's prev property to null
+//set the old head's next to null
+//decrement the length
+//return old head
+
+//unshift - adding a node at the beginning of the D.L.L
+//create a new node with the value passed to the function
+//if the length is 0
+    //set the head to be the new node
+    //set the tail to be the new node
+//otherwise
+    //set the prev property on the head of the list to be the new node
+    //set the next property on the new node to be the head property
+//update the head to be the new node
+//increment the length
+//return the list
+
+//Get - accessing a node by its position
+//if the index is less than 0 or greater or equal to the length, return null
+//if the index is less than or equal to half the length of the list
+    //loop through the list starting from the head and loop towards the middle
+    //return the node once it is found
+//if the index is greater than half the length of the list
+    //loop through the list starting from the tail and loop towards the middle
+    //return the node once it is found
+
+
 class Node {
     constructor(val){
         this.val = val;
@@ -63,9 +98,62 @@ class DoublyLinkedList{
         this.length--;
         return poppedNode;
     }
+    shift() {
+        if(this.length === 0) return undefined;
+        let oldHead = this.head;
+        if(this.length === 1){ //edge case
+            this.head = null;
+            this.tail = null;
+        } else { 
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    unshift(val){
+        let newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(index){
+        if(index < 0 || index >= this.length) return null;//check for upper and lower bound(on either sides)
+        let current;
+        if(index <= this.length/2 ){ //when the index is below the middle we start from the head
+            let count = 0;
+            current = this.head;
+            while(count != index){
+                current = current.next;
+                count++;
+            }
+        } else { //when the index is above the middle we start from the tail
+            let count = this.length - 1;
+            current = this.tail;
+            while(count !== index) {
+                current = current.prev; //stepping backwards
+                count--;
+            }   
+        }
+        return current;
+            
+    }
 }
 
 list = new DoublyLinkedList()
 list.push("99");
 list.push("100");
-console.log(list.pop())
+list.push("120");
+// console.log(list.pop());
+// console.log(list.shift());
+// console.log(list.unshift("150"));
+console.log(list.get(1));
+// console.log(list)
